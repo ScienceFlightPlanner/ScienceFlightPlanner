54a1e3d3ac2a749ce6503e449d5e085bfd65c57b
Maxim <maxim.poliakov@stud.tu-darmstadt.de>
2024-12-07 00:31:27 +0100

    Implement meeting Feedback and fix field creation
    Signed-off-by: Maxim <maxim.poliakov@stud.tu-darmstadt.de>

--- waypoint_tag_module.py
+++ waypoint_tag_module.py
@@ -1,19 +1,22 @@
 from PyQt5.QtCore import QVariant
 from PyQt5.QtWidgets import QInputDialog
-from qgis.core import QgsWkbTypes, QgsField, QgsFieldConstraints
+from qgis.core import Qgis, QgsWkbTypes, QgsField
 from qgis.gui import QgisInterface
-
 
 from .utils import LayerUtils
 
 class WaypointTagModule:
-    tags = ["Fly-by",
-            "Fly-over",
+    qgis_field_name = "tag"
+
+    tags = ["Fly-over",
+            "Fly-by",
             "RH 180",
             "RH 270",
             "LH 180",
             "LH 270",
             "Custom tag"]
+
+    default_tag = tags[0]
 
     iface: QgisInterface
     layer_utils: LayerUtils
@@ -29,36 +32,36 @@ class WaypointTagModule:
         if selected_layer is None:
             return
 
-        selected_features = selected_layer.getSelectedFeatures()
-        selected_layer.startEditing()
-        selected_layer.beginEditCommand("Reverse Waypoints")
-        print(selected_layer.fields())
-        if selected_layer.fields().indexFromName("tag") == -1:
-            constraint = QgsFieldConstraints()
-            constraint.setConstraintExpression(f'{len(tag)} <= 25', "length <= 25")
-            new_field = QgsField("tag", QVariant.String)
-            new_field.setConstraints(constraint)
+        if selected_layer.fields().indexFromName(self.qgis_field_name) == -1:
+            new_field = QgsField(self.qgis_field_name, QVariant.String)
             added = selected_layer.dataProvider().addAttributes([new_field])
             if added:
                 selected_layer.updateFields()
 
-        #selected_points = [f.geometry().asPoint() for f in selected_features]
-        for f in selected_features:
-            #f.attributes()
-            print(f.attributes())
-            print(f.fields())
-            f.setAttribute("tag", tag)
-            print(f.attributes())
-            print(f.fields())
-            selected_layer.updateFeature(f)
-        selected_layer.endEditCommand()
-        # update layer and commit changes
-        selected_layer.updateExtents()
-        selected_layer.commitChanges()
-        selected_layer.reload()
-        print(tag)
+            attr_map = {}
+            field_id = selected_layer.fields().indexFromName(self.qgis_field_name)
+            for feature in selected_layer.getFeatures():
+                attr_map[feature.id()] = {field_id: self.default_tag}
+
+            selected_layer.dataProvider().changeAttributeValues(attr_map)
+            selected_layer.updateFields()
+
+        selected_features = selected_layer.getSelectedFeatures()
+        selected_layer.startEditing()
+        selected_layer.beginEditCommand("Add Tag for Waypoints")
+        for feature in selected_features:
+            feature.setAttribute(self.qgis_field_name, tag)
+
+        selected_layer.removeSelection()
+        selected_layer.endEditCommand() #"Add Tag for Waypoints"
 
     def new_tag(self, parent):
         text, _ = QInputDialog.getText(parent, "Custom tag", "Enter name for custom tag:")
-        print("entered text: "+text)
+        if len(text) > 10:
+            self.iface.messageBar().pushMessage(
+                "Tag must be less than 10 characters",
+                level=Qgis.Warning,
+                duration=4,
+            )
+            return
         self.tag(text)
