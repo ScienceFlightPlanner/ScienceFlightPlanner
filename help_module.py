ce364e18f7809a425d98864d0e150edc9bdb66db
MaximPLV <maxim.poliakov@stud.tu-darmstadt.de>
2024-12-16 13:29:37 +0100

    feat: move toolbar buttons to new row, if width gets too small
    Signed-off-by: MaximPLV <maxim.poliakov@stud.tu-darmstadt.de>

--- help_module.py
+++ help_module.py
@@ -2,6 +2,8 @@ import codecs
 import os
 from typing import List, Union
 
+from PyQt5.QtGui import QResizeEvent
+from PyQt5.QtWidgets import QWidgetAction, QGridLayout, QBoxLayout, QScrollArea
 from qgis.gui import QgisInterface
 from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtWidgets import (
@@ -29,6 +31,9 @@ class HelpWidget(QDockWidget):
 
     iface: QgisInterface
     plugin_dir: str
+    layout: QBoxLayout
+    toolbars: List[QToolBar]
+    adjusted: bool
     action_module: ActionModule
     sensor_combobox_plugin: QComboBox
 
@@ -38,12 +43,14 @@ class HelpWidget(QDockWidget):
 
     toolbar_widgets: List[QWidget]
     sensor_combobox: Union[QComboBox, None]
+    separator_widget: QWidget
     text_widget: QWidget
 
-    default_action: QAction
     current_action_name: str
 
-    toolbar: QToolBar
+    last_action_index: int
+    current_width: int
+    toolbar_thresholds: List[int]
 
     def __init__(
         self,
@@ -56,9 +63,13 @@ class HelpWidget(QDockWidget):
         self.iface = iface
         self.plugin_dir = plugin_dir
 
-        self.actions = actions
+        self.toolbars = []
+        self.adjusted = True
+
         self.action_module = ActionModule(iface)
+        self.actions = []
         self.default_action = None
+        self.init_actions(actions)
 
         self.sensor_combobox_plugin = sensor_combobox
         self.sensor_combobox = None
@@ -70,6 +81,10 @@ class HelpWidget(QDockWidget):
 
         self.init_gui()
 
+        self.current_width = self.width()
+        print("Width Self: " + str(self.current_width))
+        self.toolbar_thresholds = []
+
     def init_gui(self):
         """inits gui, i.e. init toolbar and sets default action"""
         self.init_toolbar()
@@ -79,56 +94,144 @@ class HelpWidget(QDockWidget):
         if self.default_action is not None:
             self.default_action.trigger()
 
-        self.setMinimumWidth(self.toolbar.width() + 75)
+        self.setMinimumWidth(self.toolbars[0].width()//2 + 1)
         self.setFeatures(QDockWidget.DockWidgetClosable)
 
         self.iface.addDockWidget(Qt.RightDockWidgetArea, self)
 
     def close(self):
         """removes help widget from the dock widgets and disconnects signals"""
-        for action in self.actions:
-            action.setCheckable(False)
+        #for action in self.actions:
+            #action.setCheckable(False)
+
         self.iface.removeDockWidget(self)
 
     def update_widget(self):
         """updates text widget"""
-        widget = QWidget()
-        layout = QVBoxLayout()
-        layout.addWidget(self.toolbar)
-        layout.addWidget(self.text_widget)
-        widget.setLayout(layout)
+        widget = QWidget(self)
+        self.layout = QVBoxLayout(widget)
+        for toolbar in self.toolbars:
+            self.layout.addWidget(toolbar)
+
+        self.layout.addWidget(self.text_widget)
+        widget.setLayout(self.layout)
         self.setWidget(widget)
 
     def init_toolbar(self):
         """inits toolbar of the help widget"""
-        self.toolbar = QToolBar()
+        self.toolbars.append(QToolBar("Help Widget Toolbar 0", self.iface.mainWindow()))
+        self.toolbars[0].setObjectName("Help Widget Toolbar 0")
 
-        self.add_actions_toolbar(self.actions)
-        self.add_widgets_toolbar()
+        self.toolbars.append(QToolBar("Help Widget Toolbar 1", self.iface.mainWindow()))
+        self.toolbars[1].setObjectName("Help Widget Toolbar 1")
+        self.toolbars[1].setVisible(False)
 
-    def add_actions_toolbar(self, actions: List[Union[QAction, QToolButton]]):
-        """adds actions to toolbar and sets default action to 'complete manual'"""
-        assert self.toolbar is not None
+        self.add_actions_to_toolbar()
+        self.add_widgets_to_toolbar()
+        self.last_action_index = len(self.actions) - 1
+
+    def init_actions(self, actions: List[Union[QAction, QToolButton]]):
         for action in actions:
             # set triggers so that when pressing the button the corresponding html text shows
             fct = self.get_corresponding_action_fct(action)
             action.triggered.connect(fct)
             action.setCheckable(True)
-            self.toolbar.addAction(action)
 
             # default action is set to the action displaying the complete manual
             if action.text() == complete_manual_name:
                 self.default_action = action
 
-    def add_widgets_toolbar(self):
-        """adds widgets in widgets list to the toolbar"""
-        assert self.toolbar is not None
-        self.add_sensor_widget()
-        self.add_separator()
-        self.add_faq_widget()
+            self.actions.append(action)
 
-        for w in self.toolbar_widgets:
-            self.toolbar.addWidget(w)
+    def adjust_tool_bars_to_decreasing_width(self):
+        print("adjust dec")
+        action_to_be_moved = self.actions[self.last_action_index]
+
+        self.toolbars[0].removeAction(action_to_be_moved)
+
+        before_action = (
+            None
+            if self.last_action_index + 1 == len(self.actions)
+            else self.actions[self.last_action_index + 1]
+        )
+
+        self.toolbars[1].insertAction(before_action, action_to_be_moved)
+        action_to_be_moved.setParent(self.toolbars[1])
+
+        if self.last_action_index == len(self.actions) - 1:
+            self.toolbars[1].setVisible(True)
+            self.layout.insertWidget(1, self.toolbars[1])
+
+        self.last_action_index -= 1
+
+        self.current_width = self.width()
+
+        self.toolbar_thresholds.append(self.current_width)
+
+        if len(self.toolbars[1].actions()) == len(self.actions) // 2:
+            self.setMinimumWidth(self.width())
+
+    def adjust_tool_bars_to_increasing_width(self):
+        action_to_be_moved = self.actions[self.last_action_index + 1]
+
+        self.toolbars[1].removeAction(action_to_be_moved)
+
+        before_action = None if self.last_action_index >= 0 else self.actions[self.last_action_index]
+
+        self.toolbars[0].insertAction(before_action, action_to_be_moved)
+        action_to_be_moved.setParent(self.toolbars[0])
+
+        if not self.toolbars[1].actions():
+            self.toolbars[1].setVisible(False)
+            self.layout.removeWidget(self.toolbars[1])
+
+        del self.toolbar_thresholds[-1]
+
+        self.last_action_index += 1
+
+        print("adjust inc")
+
+    def add_actions_to_toolbar(self):
+        """adds actions to toolbar and sets default action to 'complete manual'"""
+        for action in self.actions:
+            action.setParent(self.toolbars[0])
+            self.toolbars[0].addAction(action)
+
+    def add_actions_toolbar2(self, actions: List[Union[QAction, QToolButton]]):
+        """adds actions to toolbar and sets default action to 'complete manual'"""
+
+        #toolbar = self.iface.mainWindow().findChild(QToolBar, "ScienceFlightPlanner")
+        #tool_buttons = [x for x in toolbar.findChildren(QToolButton) if x.objectName() != "qt_toolbar_ext_button"]
+        grid_widget = QWidget()
+        layout = QGridLayout(grid_widget)
+        i = 0
+        assert self.toolbars[0] is not None
+        for t in actions:
+            t.setParent(self.toolbars[0])
+            fct = self.get_corresponding_action_fct(t)
+            print(fct)
+            t.clicked.connect(fct)
+            t.setCheckable(True)
+            layout.addWidget(t, i // 6, i % 6)
+            if t.text() == "Help":
+                self.default_action = t
+            i+=1
+
+        self.toolbars[0].addWidget(grid_widget)
+
+        #self.toolbar = self.iface.mainWindow().findChild(QToolBar, "mSelectionToolBar")
+        #self.iface.mainWindow().findChild(QToolBar, "mSelectionToolBar").findChild(QToolButton, "qt_toolbar_ext_button")
+
+    def add_widgets_to_toolbar(self):
+        """adds widgets in widgets list to the toolbar"""
+        assert self.toolbars[0] is not None
+
+        sensor_widget_action = SensorWidgetAction(self)
+        faq_widget_action = FAQWidgetAction(self)
+        self.toolbars[0].addAction(sensor_widget_action)
+        self.toolbars[0].addAction(faq_widget_action)
+        self.actions.append(sensor_widget_action)
+        self.actions.append(faq_widget_action)
 
     def add_sensor_widget(self):
         """adds a sensor widget to the toolbar widgets (widgets are drawn in the order that they are added to the widgets list)"""
@@ -157,7 +260,7 @@ class HelpWidget(QDockWidget):
 
         separator_widget = QWidget()
         separator_widget.setLayout(layout)
-        self.toolbar_widgets.append(separator_widget)
+        return separator_widget
 
     def add_faq_widget(self):
         """adds a faq widget to the toolbar widgets (widgets are drawn in the order that they are added to the widgets list)"""
@@ -305,6 +408,22 @@ class HelpWidget(QDockWidget):
         """trigger function for 'reverse waypoints'"""
         self.fct_action(self.action_module.reversal)
 
+    def resizeEvent(self, event: QResizeEvent):
+        super().resizeEvent(event)
+        toolbar_extension_button_2 = self.toolbars[1].findChild(QToolButton, "qt_toolbar_ext_button")
+        if toolbar_extension_button_2 and toolbar_extension_button_2.isVisible():
+            self.setMinimumWidth(event.oldSize().width())
+            return
+
+        toolbar_extension_button = self.toolbars[0].findChild(QToolButton, "qt_toolbar_ext_button")
+        if toolbar_extension_button and toolbar_extension_button.isVisible():
+            toolbar_extension_button.hide()
+            self.adjust_tool_bars_to_decreasing_width()
+        elif self.isVisible():
+            toolbar_threshold_index = len(self.actions) - 2 - self.last_action_index
+            if (0 <= toolbar_threshold_index < len(self.toolbar_thresholds)
+                    and event.size().width() > self.toolbar_thresholds[toolbar_threshold_index]):
+                self.adjust_tool_bars_to_increasing_width()
 
 class HelpManualModule:
 
@@ -351,10 +470,11 @@ class HelpManualModule:
         """closes the module and disconnects signals"""
         assert self.help_action is not None and self.actions is not None
         if self.help_widget is not None:
-            self.help_widget.visibilityChanged.disconnect(self.close)
+            # TODO: the following Line throws "TypeError 'method' object is not connected"
+            #       is this line even necessary?
+            #self.help_widget.visibilityChanged.disconnect(self.close)
             self.help_action.setChecked(False)
             self.help_widget.close()
-            self.help_widget = None
 
     def set_actions(self):
         """copies the given lists of actions and modifies it so that the help action corresponds to the complete manual actions"""
@@ -382,7 +502,6 @@ class HelpManualModule:
                 action.setToolTip(complete_manual_name)
                 # assure that complete manual action is the first action in the help widget
                 self.actions.insert(0, action)
-            print(action.toolTip())
 
 
 def create_text_widget(html_str: str = "") -> QWidget:
@@ -406,7 +525,12 @@ def set_checked_for_corresponding_action_button(
     action_txt: str, actions: List[Union[QAction, QToolButton]], checked: bool
 ):
     """sets the action button with the given text to checked/not checked depending on the parameter checked"""
+    # TODO QPushButton (faq button)  keeps getting deleted, which throws an error here
     matching_actions = [action for action in actions if action.text() == action_txt]
+    print("-------------")
+    for action in matching_actions:
+        print(str(action) + " : " + str(action.toolTip()))
+    print("-------------")
     # if multiple action or no action matches no action is set checked
     if len(matching_actions) != 1:
         return
@@ -417,3 +541,69 @@ def set_checked_for_corresponding_action_button(
         return
 
     action.setChecked(checked)
+
+class SensorWidgetAction(QWidgetAction):
+    help_widget: Union[HelpWidget, None]
+
+    def __init__(self, help_widget: HelpWidget):
+        super().__init__(help_widget.toolbars[0])
+        self.help_widget = help_widget
+        self.setObjectName("SensorWidgetAction")
+        super().setObjectName("SensorWidgetAction")
+
+    def createWidget(self, parent):
+        layout = QHBoxLayout()
+        layout.setContentsMargins(2, 0, 0, 0)
+
+        sensor_combobox = QComboBox()
+        sensor_combobox.setToolTip(self.help_widget.action_module.sensor_coverage)
+        default_icon_height = self.help_widget.toolbars[0].iconSize().height()
+        sensor_combobox.setFixedHeight(default_icon_height)
+        layout.addWidget(sensor_combobox)
+
+        sensor_combobox.addItem(self.help_widget.sensor_combobox_plugin.currentText())
+        sensor_combobox.highlighted.connect(self.help_widget.fct_display_coverage)
+        sensor_combobox.activated.connect(self.help_widget.update_coverage_widget)
+        self.help_widget.sensor_combobox = sensor_combobox
+
+        sensor_widget = QWidget(parent)
+        sensor_widget.setLayout(layout)
+        sensor_widget.setObjectName("SensorWidgetAction")
+        return sensor_widget
+
+class FAQWidgetAction(QWidgetAction):
+    help_widget: Union[HelpWidget, None]
+
+    def __init__(self, help_widget: HelpWidget):
+        super().__init__(help_widget.toolbars[0])
+        self.help_widget = help_widget
+        self.setObjectName("FAQWidgetAction")
+        super().setObjectName("FAQWidgetAction")
+
+    def createWidget(self, parent):
+        layout = QHBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        separator_layout = QHBoxLayout()
+        separator_layout.setContentsMargins(15, 0, 10, 0)
+        separator_label = QLabel("|")
+        separator_layout.addWidget(separator_label)
+        separator_widget = QWidget()
+        separator_widget.setLayout(separator_layout)
+        self.help_widget.separator_widget = separator_widget
+        layout.addWidget(separator_widget)
+
+        faq_button = QPushButton(faq_action_name)
+        faq_button.setToolTip("faq")
+        faq_button.setAutoDefault(False)
+        faq_button.setCheckable(True)
+        faq_button.clicked.connect(self.help_widget.fct_faq)
+        default_icon_height = self.help_widget.toolbars[0].iconSize().height()
+        faq_button.setFixedHeight(default_icon_height)
+        self.help_widget.faq_button = faq_button
+        layout.addWidget(faq_button)
+
+        faq_widget = QWidget(parent)
+        faq_widget.setLayout(layout)
+        faq_widget.setObjectName("FAQWidgetAction")
+        return faq_widget
