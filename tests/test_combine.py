608c60eb5a3a8ba32ae3c627749e8aed9f2f7c3c
MaximPLV <maxim.poliakov@stud.tu-darmstadt.de>
2025-02-14 23:02:23 +0100

    update combine tests
    Signed-off-by: MaximPLV <maxim.poliakov@stud.tu-darmstadt.de>

--- tests/test_combine.py
+++ tests/test_combine.py
@@ -1,7 +1,9 @@
 import os
 import sys
+import pandas
 
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox, QPushButton
+from qgis._core import QgsProject
 from qgis.testing import unittest
 from parameterized import parameterized
 from unittest.mock import patch
@@ -27,6 +29,8 @@ from ScienceFlightPlanner.tests.utils import (
 from ScienceFlightPlanner.science_flight_planner import ScienceFlightPlanner
 # noinspection PyUnresolvedReferences
 from ScienceFlightPlanner.combine_flightplans_module import CombineFlightplansModule
+# noinspection PyUnresolvedReferences
+from ScienceFlightPlanner.constants import QGIS_FIELD_NAME_ID, QGIS_FIELD_NAME_TAG
 
 TAG_LIST = TAGS + [CUSTOM_TAG]
 
@@ -38,32 +42,50 @@ class TestCombine(BaseTest):
         self.combine_flightplans_module = self.plugin_instance.combine_flightplans_module
 
     @parameterized.expand([
-        ["a", "Flight1", 10, "Flight3", 10, "Flight1_3_combined"],
-        ["b", "Flight1", 1, "Flight3", 1, "Flight1_3_combined"],
-        ["c", "Flight1", 10, "Flight3", 10, "Flight1_3_combined"],
+        ["Concatenate_F1_F3", "Flight1", 21, "Flight3", 1, "Flight1", "Flight1_3_combined", "resources/Flight1_3_combined_wp21_1_reference.csv"],
+        ["Concatenate_F3_F1", "Flight1", 1, "Flight3", 81, "Flight3", "Flight3_1_combined", "resources/Flight3_1_combined_wp81_1_reference.csv"],
+        ["Combine_F1_F3_at_10_10", "Flight1", 10, "Flight3", 10, "Flight1", "Flight1_3_combined", "resources/Flight1_3_combined_wp10_10_reference.csv"],
+        ["Combine_F3_F1_at_10_10", "Flight1", 10, "Flight3", 10, "Flight3", "Flight3_1_combined", "resources/Flight3_1_combined_wp10_10_reference.csv"],
     ])
-    @patch("PyQt5.QtWidgets.QMessageBox.question")
+    @patch("PyQt5.QtWidgets.QMessageBox.addButton")
+    @patch("PyQt5.QtWidgets.QMessageBox.exec")
+    @patch("PyQt5.QtWidgets.QMessageBox.clickedButton")
     @patch("ScienceFlightPlanner.utils.LayerUtils.get_shp_file_path")
-    def test_template(self, name, layer1_name, layer1_wp_id, layer2_name, layer2_wp_id, result_layer_name, mock_question_box, mock_get_shp_file_path):
-        mock_get_shp_file_path.return_value = "/tests_directory/ScienceFlightPlanner/tests/"
-        def side_effect(_0, path_of_current_layer, path_suffix):
+    def test_combine(self, name, layer1, layer1_wp_id, layer2, layer2_wp_id, start_layer, result_layer_name, expected, mock_get_shp_file_path, mock_clicked_button, mock_exec, mock_add_button):
+        def side_effect1(_0, path_of_current_layer, path_suffix):
             suggested_path, _ = os.path.splitext(path_of_current_layer)
             suggested_path += path_suffix
             file_path = self.plugin_instance.layer_utils.validate_file_path(suggested_path, ".shp")
             return file_path
 
-        mock_question_box.side_effect = side_effect
+        button1 = QPushButton
+        button2 = QPushButton()
 
-        mock_get_shp_file_path.return_value = QMessageBox.Yes
+        def side_effect2(*args):
+            if isinstance(args[0], str) and start_layer == layer2:
+                button2.setText(args[0])
+                return button2
+            return button1
 
-        select_layer(layer1_name)
-        select_features([layer1_wp_id])
-        select_layer(layer2_name)
-        select_features([layer2_wp_id])
+        def side_effect3():
+            return button2
+
+        mock_get_shp_file_path.side_effect = side_effect1
+
+        mock_add_button.side_effect = side_effect2
+
+        mock_clicked_button.side_effect = side_effect3
+
+        mock_exec.return_value = None
+
+        select_layer(layer1)
+        select_features([layer1_wp_id - 1])
+        select_layer(layer2)
+        select_features([layer2_wp_id - 1])
         self.combine_flightplans_module.combine()
 
-        layer1_name = get_layer(layer1_name)
-        layer2_name = get_layer(layer2_name)
+        layer1 = get_layer(layer1)
+        layer2 = get_layer(layer2)
         combined_layer = get_layer(result_layer_name)
 
         num_of_tests_passed = 0
@@ -74,11 +96,32 @@ class TestCombine(BaseTest):
         with self.subTest("Correct number of features"):
             self.assertEqual(
                 combined_layer.featureCount(),
-                layer1_name.featureCount() + layer2_name.featureCount()
+                layer1.featureCount() + layer2.featureCount()
             )
             num_of_tests_passed = increment_if_test_passed(num_of_tests_passed)
 
-        if num_of_tests_passed == 2:
+        with self.subTest("Correct waypoint order"):
+            csv = pandas.read_csv(expected)
+            for i, feature in enumerate(combined_layer.getFeatures()):
+                id = feature.attribute(QGIS_FIELD_NAME_ID)
+                reference_id = i + 1
+                self.assertEqual(id, reference_id)
+
+                tag = feature.attribute(QGIS_FIELD_NAME_TAG)
+                reference_tag = csv.at[i, "tag"]
+                self.assertEqual(reference_tag, tag)
+
+                latitude = feature.geometry().asPoint().y()
+                reference_latitude = csv.at[i, "Y"]
+                self.assertAlmostEqual(latitude, reference_latitude, delta = 0.0000000000001)
+
+                longitude = feature.geometry().asPoint().x()
+                reference_longitude = csv.at[i, "X"]
+                self.assertAlmostEqual(longitude, reference_longitude, delta = 0.0000000000001)
+
+            num_of_tests_passed = increment_if_test_passed(num_of_tests_passed)
+
+        if num_of_tests_passed == 3:
             delete_layer(combined_layer)
 
 def run_all():
